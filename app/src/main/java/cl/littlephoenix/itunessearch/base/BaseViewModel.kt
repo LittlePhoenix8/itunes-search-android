a668c248d0cc914a211278a25e4c4fc1d0e06809
Viviana <vivianagarridos@gmail.com>
2019-03-17 18:00:22 -0300

    add base view model with retrofit

---
+++ app/src/main/java/cl/littlephoenix/itunessearch/base/BaseViewModel.kt
@@ -0,0 +1,19 @@
+package cl.littlephoenix.itunessearch.base
+
+import android.arch.lifecycle.ViewModel
+import cl.littlephoenix.itunessearch.network.ItunesNetwork
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class BaseViewModel: ViewModel()
+{
+    fun getRetrofit(): ItunesNetwork
+    {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(ItunesNetwork.URL_BASE)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        return retrofit.create(ItunesNetwork::class.java)
+    }
+}
